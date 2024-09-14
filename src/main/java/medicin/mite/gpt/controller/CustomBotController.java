3b2f088c05d6c17bf2cbe774e346eb5e1175bc75
seunggyu <seungg8361@nave.com>
2024-09-14 16:01:39 +0900

    2024-09-05 로그인 회원가입 기능 구현 -완-

---
+++ src/main/java/medicin/mite/gpt/controller/CustomBotController.java
@@ -0,0 +1,32 @@
+package medicin.mite.gpt.controller;
+
+import medicin.mite.gpt.dto.ChatGPTRequest;
+import medicin.mite.gpt.dto.ChatGPTResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/bot")
+public class CustomBotController {
+    @Value("${openai.api.model}")
+    private String model;
+
+    @Value("${openai.api.url}")
+    private String apiURL;
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam(name = "prompt")String prompt){
+        ChatGPTRequest request = new ChatGPTRequest(model, prompt);
+        ChatGPTResponse chatGPTResponse =  template.postForObject(apiURL, request, ChatGPTResponse.class);
+        return chatGPTResponse.getChoices().get(0).getMessage().getContent();
+    }
+
+}
---
+++ src/main/java/medicin/mite/gpt/dto/Message.java
@@ -0,0 +1,14 @@
+package medicin.mite.gpt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String role;
+    private String content;
+
+}
